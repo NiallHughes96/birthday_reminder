07a1b8cb973eefd3c07cbfc3a169efbdb942056b
niallhughes <niall.hughes@saltdna.com>
2022-09-08 11:38:46 +0100

    Display Title Card and list of sample data

--- app/src/main/java/com/example/birthdayreminder/MainActivity.kt
+++ app/src/main/java/com/example/birthdayreminder/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.example.birthdayreminder
 
+import android.content.res.Configuration
+import android.icu.text.BidiRun
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,42 +31,96 @@ class MainActivity : ComponentActivity() {
             BirthdayReminderTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxWidth(),
                 ) {
-                    Title(profileInfo = ProfileInfo("Niall", "Welcome "))
+                    Column {
+                        Title(profileInfo = ProfileInfo("Niall"))
+                        UpcomingBirthdays(birthdays = SampleData.birthdaysSample)
+                    }
+
                 }
             }
         }
     }
 }
 
-data class ProfileInfo(val user: String, val message: String)
+data class ProfileInfo(val user: String)
+data class UpcomingBirthday(val name: String, val date: String)
 
 @Composable
-fun Title(profileInfo: ProfileInfo) {
-    Column {
+fun Title(profileInfo: ProfileInfo){
+    Column (horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth().padding(8.dp)){
         Image(
             painter = painterResource(id = R.drawable.birthday_cake),
             contentDescription = "Birthday cake image",
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape),
+            contentScale = ContentScale.FillHeight
         )
 
-        Spacer(modifier = Modifier.width(8.dp))
-
-        Row (modifier = Modifier.padding(8.dp)){
-            Text(text = profileInfo.message)
-            Text(text = profileInfo.user)
+        Row (){
+            Text(text = "Welcome ")
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp){
+                Text(text = profileInfo.user, color = MaterialTheme.colors.secondaryVariant)
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Birthday(upB: UpcomingBirthday) {
+    Row (verticalAlignment = Alignment.CenterVertically){
+        Image(
+            painter = painterResource(id = R.drawable.birthday_cake),
+            contentDescription = "Birthday cake image",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+                .padding(8.dp),
+            contentScale = ContentScale.FillHeight
+        )
+        Column (modifier = Modifier.padding(8.dp)){
+            Text(text = upB.name)
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp){
+                Text(text = upB.date, color = MaterialTheme.colors.secondaryVariant)
+            }
+        }
+    }
+}
+
+@Composable
+fun UpcomingBirthdays(birthdays: List<UpcomingBirthday>) {
+    LazyColumn{
+        items(birthdays.size) { upcomingBirthday ->
+            Birthday(birthdays[upcomingBirthday])
+        }
+    }
+}
+
+@Preview(name = "Light Mode", showBackground = true)
+//@Preview(
+//    uiMode = Configuration.UI_MODE_NIGHT_YES,
+//    showBackground = true,
+//    name = "Dark Mode"
+//)
 @Composable
 fun PreviewTitle() {
     BirthdayReminderTheme {
-        Title(profileInfo = ProfileInfo("Niall" + "!", "Welcome "))
+        Surface{
+            Title(profileInfo = ProfileInfo("Niall" + "!"))
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun PreviewBirthdayList() {
+    BirthdayReminderTheme() {
+        Surface{
+            UpcomingBirthdays(birthdays = SampleData.birthdaysSample)
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/birthdayreminder/SampleData.kt
@@ -0,0 +1,32 @@
+package com.example.birthdayreminder
+
+object SampleData {
+    // Sample conversation data
+
+    val birthdaysSample = listOf(
+        UpcomingBirthday("Caitlin", "September 3rd"),
+        UpcomingBirthday("wwwwww", "eeeeeee"),
+        UpcomingBirthday("Caitlin", "September 3rd"),
+        UpcomingBirthday("vcnbfvngfn", "98989898 3rd"),
+        UpcomingBirthday("Caitlin", "September 3rd"),
+        UpcomingBirthday("sdcds", "Sdsvscdsc 3rd"),
+        UpcomingBirthday("Caitlin", "September 3rd"),
+        UpcomingBirthday("sdccsd", "5465465 3rd"),
+        UpcomingBirthday("Caitlin", "September 3rd"),
+        UpcomingBirthday("Caitlin", "September 3rd"),
+        UpcomingBirthday("Caitlin", "September 3rd"),
+        UpcomingBirthday("lastksancdjs", "10000900"),
+        UpcomingBirthday("Caitlin", "September 3rd"),
+        UpcomingBirthday("wwwwww", "eeeeeee"),
+        UpcomingBirthday("Caitlin", "September 3rd"),
+        UpcomingBirthday("vcnbfvngfn", "98989898 3rd"),
+        UpcomingBirthday("Caitlin", "September 3rd"),
+        UpcomingBirthday("sdcds", "Sdsvscdsc 3rd"),
+        UpcomingBirthday("Caitlin", "September 3rd"),
+        UpcomingBirthday("sdccsd", "5465465 3rd"),
+        UpcomingBirthday("Caitlin", "September 3rd"),
+        UpcomingBirthday("Caitlin", "September 3rd"),
+        UpcomingBirthday("Caitlin", "September 3rd"),
+        UpcomingBirthday("lastksancdjs", "10000900"),
+    )
+}
